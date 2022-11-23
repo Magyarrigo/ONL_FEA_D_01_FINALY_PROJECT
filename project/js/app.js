c43771120612d3aa23bfdde1a5429c7305fa6e45
Magyarrigo <b.drozd07@gmail.com>
2022-11-23 19:23:19 +0100

    Solved_Openning

--- project/js/app.js
+++ project/js/app.js
@@ -12,3 +12,35 @@ hamburgerMenu.addEventListener('click',function (){
 po nacisnieciu hamburgera pojawia sie menu w wersji mobilnej
 oraz x do zamkn iecia menu
  */
+
+//Przygotowanie pokazu slidow
+var slideIndex = 1;
+showSlide(slideIndex);
+
+const leftArrow = document.querySelector('.openning__Button--Left');
+const rightArrow = document.querySelector('.openning__Button--Right');
+leftArrow.addEventListener('click',function(event){
+    slideIndex += (-1);
+    showSlide(slideIndex)
+});
+rightArrow.addEventListener('click',function (event){
+    slideIndex += 1;
+    showSlide(slideIndex)
+});
+
+function showSlide(n){
+    var i = 0;
+    var slidesArray = document.querySelectorAll('.openning__Slides');
+    //console.log(slidesArray);
+    if (n > slidesArray.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slidesArray.length} ;
+    for (i = 0; i < slidesArray.length; i++) {
+        slidesArray[i].style.display = "none";
+    }
+    slidesArray[slideIndex-1].style.display = "block";
+}
+//koniec funkcji pookazu slidow
+
+
+
+
